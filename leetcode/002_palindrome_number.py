933fb548a75c830c119c0e61b1df573748179892
Rahul degra <rahuldegar28@gmail.com>
2026-09-10 12:08:09 +0530

    Optimize Palindrome Number using modulo and floor division (no string conversion)

--- leetcode/002_palindrome_number.py
+++ leetcode/002_palindrome_number.py
@@ -1,5 +1,18 @@
 def isPalindrome(x):
-    return str(x) == str(x)[::-1]
-print(isPalindrome(121))   # Expected: True
-print(isPalindrome(-121))  # Expected: False
-print(isPalindrome(10))    # Expected: False
+    if x < 0:
+        return False
+    if x > 0 and x % 10 == 0:
+        return False
+    temp = x
+    reversed_num = 0
+
+    while temp > 0:
+    
+        digit = temp % 10
+        reversed_num = (reversed_num * 10) + digit
+        temp = temp // 10
+    return reversed_num == x    
+            
+print(isPalindrome(121))
+print(isPalindrome(-121))
+print(isPalindrome(10))
